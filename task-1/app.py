d26d1f1dfaa2c241c5a76ea41b233bd49e26f91d
Erfanx <irfansyed479@gmail.com>
2021-02-09 00:21:57 +0600

    Added task-1

---
+++ task-1/app.py
@@ -0,0 +1,71 @@
+from flask import Flask
+from flask import request
+import sqlite3
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    db = sqlite3.connect('todosqli.db')
+    cursor = db.cursor()
+
+    cursor.execute('SELECT * FROM todosqli')
+    data = cursor.fetchall()
+
+    db.close()
+
+    return str(data)
+
+
+@app.route('/create')
+def create():
+    db = sqlite3.connect('todosqli.db')
+    cursor = db.cursor()
+
+    UserName = request.args.get('UserName')
+    Email = request.args.get('Email')
+    PasswordHash = request.args.get('PasswordHash')
+    CreatedAt = request.args.get('CreatedAt')
+    IsActive = request.args.get('IsActive')
+    cursor.execute(
+        'INSERT INTO todosqli(UserName, Email, PasswordHash, CreatedAt, ISActive) VALUES("%s", "%s", "%s", "%f", "%d")' % (UserName, Email, PasswordHash, CreatedAt, IsActive))
+    db.commit()
+
+    db.close()
+    return 'UserName: %s  |  Email: %s | PasswordHash: %s | CreatedAt: %f | IsActive: %d' % (UserName, Email, PasswordHash, CreatedAt, IsActive)
+
+
+@app.route('/update/<_UserID>')
+def update(_UsedID):
+    db = sqlite3.connect('todosqli.db')
+    cursor = db.cursor()
+
+    UserName = request.args.get('UserName')
+    Email = request.args.get('Email')
+    PasswordHash = request.args.get('PasswordHash')
+    CreatedAt = request.args.get('CreatedAt')
+    IsActive = request.args.get('IsActive')
+
+    cursor.execute(
+        'UPDATE todosqli SET UserName= "%s", Email="%s", PasswordHash="%s", CreatedAt="%f", IsActive="%d" WHERE UserID= %d' % (UserName, Email, PasswordHash, CreatedAt, IsActive, _UserID))
+    db.commit()
+
+    db.close()
+    return '_UserID: %d  |  UserName: %s  |  Email: %s | PasswordHash: %s | CreatedAt: %f | IsActive: %d' % (_UserID, UserName, Email, PasswordHash, CreatedAt, IsActive)
+
+
+@app.route('/delete/<_UserID>')
+def delete(_UserID):
+    db = sqlite3.connect('todosqli.db')
+    cursor = db.cursor()
+
+    cursor.execute('DELETE FROM todosqli WHERE UserID=%d' % _UserID)
+    db.commit()
+
+    db.close()
+    return 'deleted _UserID: %d' % _UserID
+
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
